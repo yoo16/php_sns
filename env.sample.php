c8a4774227796d88ce6b8548e9de9174b1e81e39
yoo16 <yohei.yoshikawa@gmail.com>
2024-04-14 23:05:14 +0900

    update

--- env.sample.php
+++ env.sample.php
@@ -16,6 +16,7 @@ const BASE_DIR = __DIR__;
 const APP_DIR = __DIR__ . "/app/";
 const LIB_DIR = __DIR__ . "/lib/";
 const DATA_DIR = __DIR__ . "/data/";
+const IMAGE_DIR = __DIR__ . "/images/";
 const MODEL_DIR = APP_DIR . "models/";
 const VIEW_DIR = APP_DIR . "views/";
 const CONTROLLER_DIR = APP_DIR . "controllers/";
@@ -26,11 +27,11 @@ require_once LIB_DIR . 'functions.php';
 require_once LIB_DIR . 'View.php';
 require_once LIB_DIR . 'Session.php';
 require_once LIB_DIR . 'Route.php';
+require_once LIB_DIR . 'Model.php';
 
 require_once VALIDATE_DIR . 'UserValidate.php';
 require_once VALIDATE_DIR . 'TweetValidate.php';
 
-require_once MODEL_DIR . 'Model.php';
 require_once MODEL_DIR . 'User.php';
 require_once MODEL_DIR . 'Tweet.php';
 require_once MODEL_DIR . 'Like.php';
